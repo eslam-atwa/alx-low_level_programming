f1fb508351e63a0f4be4dea180f660fa6baba2ef
Eslam Atwa <eslamatwa50@gmail.com>
2023-03-17 06:59:10 -0700

    commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - The start and end function prints numbers
+* Return: 0 in if successful
+*/
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
