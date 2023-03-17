534887dedb72a924b3212ce62e0b6fe8e31bec55
Eslam Atwa <eslamatwa50@gmail.com>
2023-03-17 08:12:08 -0700

    commit message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - The start and end function prints "a-z"
+* Return: 0 in if successful
+*/
+int main(void)
+{
+	char m;
+	char s;
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	for (s = 'a'; s <= 'f'; s++)
+	       putchar(s);
+	putchar('\n');
+	return (0);
+}
