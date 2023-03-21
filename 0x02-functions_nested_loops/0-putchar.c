40cb4cf255d7c815af364058341d5cba102d6161
eslam-atwa <eslamatwa50@gmail.com>
2023-03-21 22:13:45 +0200

    commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
- * main - evaluates an integer
- * Return: zero if excuted properly
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
-	char pu[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (i <= 8)
-		{
-		_putchar(pu[i]);
-		i++;
-		}
 	return (0);
 }
