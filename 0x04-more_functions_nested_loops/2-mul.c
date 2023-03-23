c0d1f7e8a58137b976697a14dc39ee4e6dec0e0c
eslam-atwa <eslamatwa50@gmail.com>
2023-03-23 22:11:45 +0200

    commit message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first operand.
+ * @b: secound operand.
+ *
+ * Return: 1 if uppercase 0 otherwise.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
