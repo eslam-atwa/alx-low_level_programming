1ae5b894c2ecbc2312ba6658faec0dffcd08dbbe
eslam-atwa <eslamatwa50@gmail.com>
2023-03-23 23:21:21 +0200

    commit message

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,17 @@ void more_numbers(void)
 	char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1',
 	'0', '1', '1', '1', '2', '1', '3', '1', '4'};
 	int i;
+	int n = 10;
 
-	i = 0;
-
-	while (i < 20)
+	
+	while (n--)
 	{
-		_putchar(num[i]);
-		i++;
+	i = 0;
+		while (i < 20)
+		{
+			_putchar(num[i]);
+			i++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
