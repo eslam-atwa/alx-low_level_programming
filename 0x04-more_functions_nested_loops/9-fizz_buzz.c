8a498ef3c3b999bb7dad3f7be34098fdad0abd96
eslam-atwa <eslamatwa50@gmail.com>
2023-03-24 01:36:46 +0200

    commit message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,32 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 			i++;
 			continue;
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 			i++;
 			continue;
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 			i++;
 			continue;
 		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+			i++;
+		}
 		else
-			printf("%d ", i);
-		i++;
+		{
+			printf(" %d", i);
+			i++;
+		}
 	}
 	printf("\n");
 	return (0);
