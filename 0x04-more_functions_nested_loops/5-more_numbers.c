3f15e74200c9be9b2839fce341852a9dd33ba8bb
eslam-atwa <eslamatwa50@gmail.com>
2023-03-23 23:10:42 +0200

    commit message

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+	char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '0', '1', '1', '1', '2', '1', '3', '1', '4'};
+	int i;
+
+	i = 0;
+
+	while (i < 20)
+	{
+		_putchar(num[i]);
+		i++;
+	}
+	_putchar('\n');
+}
