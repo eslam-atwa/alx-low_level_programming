306f7ec901b772313961e1799824717eec4e4a7d
eslam-atwa <eslamatwa50@gmail.com>
2023-03-24 13:55:59 +0200

    commit message

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -9,7 +9,10 @@ int main(void)
 {
     print_diagonal(0);
     print_diagonal(2);
-    print_diagonal(10);
     print_diagonal(-4);
+    print_diagonal(10);
+    print_diagonal(18);
+    print_diagonal(1);
+    
     return (0);
 }
