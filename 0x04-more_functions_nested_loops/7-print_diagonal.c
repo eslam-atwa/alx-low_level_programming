6907e96e3333371c87bda5cecbab720b5e48c56f
eslam-atwa <eslamatwa50@gmail.com>
2023-03-24 14:34:57 +0200

    commit message

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,9 @@ void print_diagonal(int n)
 	int i = n;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	while (n > 0)
 	{
 		g = i;
--- 0x04-more_functions_nested_loops/main_2.c
+++ 0x04-more_functions_nested_loops/main_2.c
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
+	print_diagonal(8);
+	print_diagonal(0);
 	print_diagonal(3);
+	print_diagonal(98);
 	return (0);
 }
+
+	
