c8e423219efdf61d7a3b92172405275f8802ca18
eslam-atwa <eslamatwa50@gmail.com>
2023-03-27 22:53:52 +0200

    commit message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse.
- *
- * @s: the string.
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
  */
 void print_rev(char *s)
 {
-	int i = 0;
+	int i, j, len;
 
-	while (s[i])
+	i = 0;
+
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	while (i+1)
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
