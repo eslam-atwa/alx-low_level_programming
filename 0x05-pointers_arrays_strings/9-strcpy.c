9e4f8d86a2fd9f4b51c199a1a9b0014bd3ac837c
eslam-atwa <eslamatwa50@gmail.com>
2023-03-28 16:25:06 +0200

    commit message

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string.
+ *
+ * @dest: will be returned.
+ * @src: will be copied.
+ *
+ * Return: Always dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len, i = 0, m;
+while (src[i])
+{
+i++;
+len++;
+}
+for(m = 0; m <= len; m++)
+{
+dest[m] = src[m];
+}
+return (dest);
+}
