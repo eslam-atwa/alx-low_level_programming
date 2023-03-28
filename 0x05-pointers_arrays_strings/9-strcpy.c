3049725922dd0c33a962013910765e8f4c2c79a1
eslam-atwa <eslamatwa50@gmail.com>
2023-03-28 16:27:07 +0200

    commit message

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,15 +10,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int len, i = 0, m;
-while (src[i])
-{
-i++;
-len++;
-}
-for(m = 0; m <= len; m++)
-{
-dest[m] = src[m];
-}
-return (dest);
+	int len, i = 0, m;
+
+	while (src[i])
+	{
+		i++;
+		len++;
+	}
+	for (m = 0; m <= len; m++)
+	{
+		dest[m] = src[m];
+	}
+
+	return (dest);
 }
