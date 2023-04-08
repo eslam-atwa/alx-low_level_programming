8b0559372652d5b538500247db8a0be90e4ded79
eslam-atwa <eslamatwa50@gmail.com>
2023-04-08 15:43:12 +0200

    commit message

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the argument count.
+ * Return: returns 0 if excuted properly.
+ * @argc: the argument count.
+ * @argv: the argument vector.
+ */
+int main(int argc, char __attribute__((unused))**argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
