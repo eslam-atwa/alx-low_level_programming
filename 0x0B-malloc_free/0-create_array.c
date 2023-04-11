7ddb97c9949c0d616b3c85d2fedbfee320b51a2f
eslam-atwa <eslamatwa50@gmail.com>
2023-04-11 19:28:14 +0200

    commit message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,12 +7,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ar = malloc(sizeof(char) * size);
+	char *ar;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
+	ar = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 
