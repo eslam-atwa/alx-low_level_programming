01c85071b1d9eada7b88dbaf8f072d337c532333
eslam-atwa <eslamatwa50@gmail.com>
2023-04-12 20:34:30 +0200

    commit message

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,18 +10,18 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	unsigned int i, size1 = 0, size2 = 0, m;
 
-	while(s1[size1])
+	while (s1[size1])
 		size1++;
-	while(s2[size2])
+	while (s2[size2])
 		size2++;
 	ar = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < (size1); i++)
 		ar[i] = s1[i];
-	m = size1;	
+	m = size1;
 	for (i = 0; i < (size2 + size1 + 1); i++)
-	{	
+	{
 		ar[m] = s2[i];
 		m++;
 	}
