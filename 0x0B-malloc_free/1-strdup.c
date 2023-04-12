e33ef987cbe865155ed10b9e1d8f4ebc994af961
eslam-atwa <eslamatwa50@gmail.com>
2023-04-12 20:37:12 +0200

    commit message

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	ar = malloc(sizeof(char) * (size + 1));
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i <= (size); i++)
+	for (i = 0; i < size; i++)
 		ar[i] = str[i];
 
 	return (ar);
