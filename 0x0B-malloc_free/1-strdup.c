b668356a4420d77e7e39c32b623fa88ea518786e
eslam-atwa <eslamatwa50@gmail.com>
2023-04-12 20:49:05 +0200

    commit message

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,33 @@
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * _strdup - duplicates a string.
- * Return: returns a pointer if successful.
- * @str: the String.
+ * *_strdup - copies the string
+ * @str: The String
+ *
+ * Return: returns pointer if succesfull.
  */
 char *_strdup(char *str)
 {
 	char *ar;
-	unsigned int i, size;
+	unsigned int i = 0, size = 0;
 
 	if (str == NULL)
 		return (NULL);
 
 	while (str[size])
-	{
 		size++;
-	}
+
 	ar = malloc(sizeof(char) * (size + 1));
+
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ar[i] = str[i];
+
+	while (i < size)
+	{
+		ar[i] = str[i]; 
+		i++;
+	}
 
 	return (ar);
 }
