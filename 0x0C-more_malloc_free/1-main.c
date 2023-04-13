348a5e279fad232b4e51204cf0908a845d318e5c
eslam-atwa <eslamatwa50@gmail.com>
2023-04-14 01:51:14 +0200

    commit message

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("test", NULL, 3);
+    concat = string_nconcat(NULL, "testring", 3);
     printf("%s\n", concat);
     free(concat);
     return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,45 +10,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *ar;
-        unsigned int i = 0, ii = 0, size1 = 0, size2 = 0;
+	char *ar;
+	unsigned int i = 0, size1 = 0, size2 = 0, ii = 0;
 
-	if (s1)
+	while (s1 && s1[size1])
+		size1++;
+	while (s2 && s2[size2])
+		size2++;
+	if (n > size2)
+		ar = malloc(sizeof(char) * (size2 + size1 + 1));
+	else
+		ar = malloc(sizeof(char) * (n + size1 + 1));
+	if (ar == NULL)
+		return (NULL);
+
+	while (i < size1)
 	{
-        	while (s1 && s1[size1])
-                	size1++;
-	}
-	if (s2)
-	{
-        	while (s2 && s2[size2])
-                	size2++;
-	}
-        if (n < size2)
-                ar = malloc(sizeof(char) * (size1 + n + 1));
-        else
-                ar = malloc(sizeof(char) * (size1 + size2 + 1));
-
-        if (ar == NULL)
-                return (NULL);
-
-        for (i = 0; i < size1; i++)
-        {
-                ar[i] = s1[i];
-                i++;
-        }
-
-	for (ii = 0; n < size2 && i < (size1 + n); ii++)
-	{
-                ar[i] = s2[ii];
+		ar[i] = s1[i];
 		i++;
 	}
-        for (ii = 0; n >= size2 && i < (size1 + size2); ii++)
+	while (ii < n)
 	{
-                ar[i] = s2[ii];
-		i++;
+		ar[i++] = s2[ii++];
 	}
+	ar[i] = '\0';
 
-        ar[i] = '\0';
-
-        return (ar);
+	return (ar);
 }
