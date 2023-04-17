f32889bc34c72c8fb3afba41de554abcdd01d5b6
eslam-atwa <eslamatwa50@gmail.com>
2023-04-17 23:28:43 +0200

    commit message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,6 +3,10 @@
 #include "dog.h"
 /**
  * init_dog - initilaizes a struct dog.
+ * @d: pointer.
+ * @name: naem.
+ * @age: age
+ * @owner: owner.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
