ede691e6d428cb59021c013fd5d95c5e5190204e
eslam-atwa <eslamatwa50@gmail.com>
2023-04-18 01:35:13 +0200

    commit message

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,16 @@
  * @age: age.
  * @owner: owner.
  */
-typedef struct
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+/**
+ * dog_t - type of dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
