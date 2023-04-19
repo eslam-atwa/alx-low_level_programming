d49f8b45fe8e8b6362e294ff49c4b75294bf2bc2
eslam-atwa <eslamatwa50@gmail.com>
2023-04-19 10:37:38 +0200

    commit message

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array: pointer to array.
+ * @size: the size.
+ * @cmp: pointer to function.
+ * Return: -1 if unsuccessful.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i= 0, c;
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		c = cmp(array[i]);
+		if (c != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
