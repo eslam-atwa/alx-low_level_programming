af171c4dd64714d3c744f47b37d2bb431228b299
eslam-atwa <eslamatwa50@gmail.com>
2023-04-19 12:19:17 +0200

    commit message

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
 /**
  * print_name - prints name.
  * @name: name.
  * @f: pointer to function.
  */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
