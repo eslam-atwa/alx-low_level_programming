ac97c40f9562975bfdd0f09be9647d0daacf5fd6
eslam-atwa <eslamatwa50@gmail.com>
2023-04-28 12:51:47 +0300

    commit message

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-        size_t s = 0;
+	size_t s = 0;
 
-        for(h = h; h != NULL; h = h->next)
-        {
-                if (!h->str)
-                        printf("[0] (nil)\n");
-                else
-                        printf("[%u] %s\n", h->len, h->str);
-                s++;
-        }
-
-        return (s);
+	for(h = h; h != NULL; h = h->next)
+	{
+		if (!h->str)
+		printf("[0] (nil)\n");
+		else
+		printf("[%u] %s\n", h->len, h->str);
+		s++;
+	}
+	
+	return (s);
 }
