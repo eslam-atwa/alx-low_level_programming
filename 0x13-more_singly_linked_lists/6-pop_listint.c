7affa0d5b46158f4ce8db87fc108152d0b230c0f
eslam-atwa <eslamatwa50@gmail.com>
2023-05-01 12:44:17 +0300

    commit message

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * pop_listint - frees the head.
+ * pop_listint - frees first element.
  * @head: pointer to the first element.
+ *
+ * Return: integer.
  */
 int pop_listint(listint_t **head)
 {
@@ -12,10 +14,10 @@ int pop_listint(listint_t **head)
 
 	if (*head == NULL || head == NULL)
 		return (0);
-	
+
 	new_h = (*head)->next;
 	i = (*head)->n;
 	free(*head);
 	*head = new_h;
-	return(i);
+	return (i);
 }
