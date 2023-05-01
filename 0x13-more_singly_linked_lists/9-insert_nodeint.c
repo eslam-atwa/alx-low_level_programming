a251b6258ed03fa4a4f214fe2d6c6800a6e5d54c
eslam-atwa <eslamatwa50@gmail.com>
2023-05-01 16:57:28 +0300

    commit message

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = elem;
 		return (elem);
 	}
-	while (*head && i <= idx)
+	while ((*head)->next && i <= idx)
 	{
 		if (i == (idx - 1))
 		{
