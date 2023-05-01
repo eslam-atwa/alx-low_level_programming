55555178d9c0d0e468cf78956c7003cd861c841c
eslam-atwa <eslamatwa50@gmail.com>
2023-05-01 17:42:07 +0300

    commit message

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *move;
 
 	if (*head == NULL || elem == NULL)
-		return (0);
+		return (NULL);
 
 	move = *head;
 	elem->n = n;
